533a831c9371a3bd9e76a638d1ace3c56a70b298
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-14 15:08:40 -0300

    cria componente de filme

---
+++ src/Filme.tsx
@@ -0,0 +1,21 @@
+export interface FilmeProps {
+    titulo: string;
+    descricao: string;
+    duracao: number;
+    classificacao: number;
+    genero: string[];
+    imagem: string;
+}
+
+export default function Filme(filme: FilmeProps) {
+    return (
+        <div>
+            <div className="titulo">{filme.titulo}</div>
+            <div className="descricao">{filme.descricao}</div>
+            <div className="duracao">{filme.duracao}</div>
+            <div className="classificacao">{filme.classificacao}</div>
+            <img className="imagem" src={filme.imagem} alt={filme.titulo}></img>
+            <div className="genero">{filme.genero}</div>
+        </div>
+    )
+}
