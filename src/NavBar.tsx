647211430c0d90138326a98f648f7258d8a2936d
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-12-14 16:55:59 -0300

    add css

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,18 +1,21 @@
+import './NavBar.css';
 
 export default function NavBar() {
   return (
     <>
-    <div>
-        <div>logo</div>
-        <div>Menu</div>
+      <div className="navbar">
+        <div className="logo">logo</div>
+        <div className="menu">Menu</div>
         <nav>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+          <ul>
+            <li>Home</li>
+            <li>Animação</li>
+            <li>Terror</li>
+            <li>comedia</li>
+            <li>Ficção</li>
+          </ul>
         </nav>
-    </div>
+      </div>
     </>
   )
 }
